04b4ce7d7d9da1a6dcb9c2c84ad13e42577f0599
bear <15662173002@163.com>
2019-06-07 16:06:47 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <template>
   <div id="app">
-    <message-toggle />
-    <list :items="['list item 1', 'listen item 2']" />
+    holle vue
   </div>
 </template>
 
 <script>
-import MessageToggle from '@/components/MessageToggle.vue'
-import List from '@/components/List.vue'
-
 export default {
-  components: {
-    MessageToggle,
-    List
-  }
+  
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-}
-</style>
